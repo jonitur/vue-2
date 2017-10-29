01a96a67d8325453c212aa02bee942e1f4cb74ee
jonitur <joniturunen@hotmail.com>
2017-10-29 10:33:46 +0200

    Added ToDo component

--- todo-app/src/components/ToDoList.vue
+++ todo-app/src/components/ToDoList.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <ul>
-        <li> Todo A </li>
-        <li> Todo B </li>
-        <li> Todo C </li>
-    </ul>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+
+    <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
-export default {
 
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
 };
 </script>
 <style>
