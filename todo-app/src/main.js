83a557055979f0582000663366407d069e76af6a
jonitur <joniturunen@hotmail.com>
2017-11-01 19:00:15 +0200

    Changed Semantic UI to local npm installation

--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -2,6 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import '../semantic/dist/semantic.css';
+import '../semantic/dist/components/dimmer';
+import '../semantic/dist/components/transition';
+import '../semantic/dist/components/dropdown';
+import '../semantic/dist/components/modal';
+import '../semantic/dist/components/rating';
+import '../semantic/dist/components/tab';
+import '../semantic/dist/components/popup';
+import '../semantic/dist/components/sticky';
 
 Vue.config.productionTip = false;
 
